b2508d379e365879a4606b21e7b091d450b2c420
HeadjuiceOne <78745732+HeadjuiceOne@users.noreply.github.com>
2022-11-07 20:35:22 +0100

    start

---
+++ app/pages/about.jsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <>
+      <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div class="bg-cover bg-center mx-14">
+        
+      <h1>
+          Bienvenue sur notre site ! Vous allez ici voir toutes ses caractéristiques. <br></br>
+          Ici vous serez capable de vous renseignez sur toutes nos offres en rapport avec le monde de la science. <br></br>
+          Si vous êtes rêveur et que vous voulez décrocher la lune alors vous êtes au bon endroit ... <br></br><br></br>
+          Que les étoiles vous fassent briller !!
+      </h1>
+      </div>
+    </div>
+    </>
+  )
+}
---
+++ app/pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Footer from '../components/Footer';
+
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div class="bg-cover bg-center mx-14">
+
+          <h1 >
+            Notre site révolutionnaire
+          </h1>
+
+        </div>
+      </div>
+
+    </>
+  )
+}
